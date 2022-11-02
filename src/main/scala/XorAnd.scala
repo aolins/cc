a17288cb1066613fc15f48819f9d9be0bf9dea08
Andrejs Olins <andrejs.olins@gmail.com>
2022-11-02 22:55:47 +0200

    xorAnd

--- src/main/scala/XorAnd.scala
+++ src/main/scala/XorAnd.scala
@@ -1,25 +1,27 @@
 object XorAnd extends App {
 
 
- val tests = scala.io.StdIn.readLine().toInt
-//val tests = 1
+ //https://www.hackerearth.com/practice/codemonk/ Minimum AND xor OR
+//maximum points
+  val tests = scala.io.StdIn.readLine().toInt
+  //val tests = 1
 
   for (_ <- 1 to tests) {
     val length = scala.io.StdIn.readLine().toInt
-    val arr = scala.io.StdIn.readLine().split(" ").map(_.toLong).toSet.toArray
+    val arr = scala.io.StdIn.readLine().split(" ").map(_.toLong).toSet.toArray.sorted
     var min = Long.MaxValue
     if(arr.length < length){
       println(0)
     }else {
-      for (i <- 0 to arr.length - 1) {
-        for (j <- i + 1 to arr.length - 1) {
-          val a: Long = arr(i)
-          val b: Long = arr(j)
-          val local = (a & b) ^ (a | b)
-          if (local < min) {
-            min = local
-          }
+      for (i <- 0 to arr.length - 2) {
+        //        for (j <- i + 1 to arr.length - 1) {
+        val a: Long = arr(i)
+        val b: Long = arr(i+1)
+        val local = (a & b) ^ (a | b)
+        if (local < min) {
+          min = local
         }
+        // }
       }
       println(min)
     }
