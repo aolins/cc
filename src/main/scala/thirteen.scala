c32553e3bc407809e38b02df8719a78118e6d074
Andrejs Olins <andrejs.olins@gmail.com>
2022-11-03 11:52:12 +0200

    13

--- src/main/scala/thirteen.scala
+++ src/main/scala/thirteen.scala
@@ -1,18 +1,46 @@
-object thirteen extends App{
-  ///https://www.youtube.com/watch?v=HFGZ1-y-1No&list=PL1YS4hYJip07A-YteNUR8qTeA_wHQarDX&index=47
-// val tests = scala.io.StdIn.readLine().toInt
-  //  //val tests = 1
-  //
-  //  for (_ <- 1 to tests) {
-  //    val length = scala.io.StdIn.readLine().toLong
-  //  }
+object thirteen extends App {
 
-  var x = 0
-  for (i <- 0 to 9999){
-    if (i.toString.contains("13")){
-      x=x+1
-      println(i)
+  ///https://www.youtube.com/watch?v=HFGZ1-y-1No&list=PL1YS4hYJip07A-YteNUR8qTeA_wHQarDX&index=47
+  val tests = scala.io.StdIn.readLine().toInt
+  //val tests = 1
+
+  val modulo = 1000000009L
+
+  var cache = collection.mutable.HashMap[Long, Long]((0l, 1l), (1L, 10L), (2l, 99l), (3l, 980l), (4, 9701l))
+
+  def recur(length:Long):Long = {
+    if (cache.contains(length)){
+      cache(length)
+    }else{
+      if(length % 2 == 0){
+        val shorterHalves = recur(length/2 - 1)
+        val halves = recur(length/2)
+        val value = halves*halves % modulo - shorterHalves*shorterHalves % modulo
+        cache += ((length, value))
+        value
+      }else if (length % 3 == 0){
+        val thirds:Long = recur(length/3)
+        val shorterThirds:Long = recur(length/3-1)
+        val value = (thirds*thirds % modulo)*thirds % modulo - (shorterThirds*shorterThirds % modulo) * shorterThirds % modulo
+        cache += ((length, value))
+        value
+       }else{
+        0l
+      }
     }
   }
-  println(10000-x)
+
+
+
+  for (_ <- 1 to tests) {
+    val length = scala.io.StdIn.readLine().toLong
+
+    if (cache.contains(length)){
+      println(cache(length))
+    }else{
+
+    }
+
+  }
+
 }
