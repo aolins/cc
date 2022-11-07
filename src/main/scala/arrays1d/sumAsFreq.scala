f8728db84e9f388be2fe013007ce6597e9aabfd5
Andrejs Olins <andrejs.olins@gmail.com>
2022-11-07 08:19:44 +0200

    freq sum 3 times out

--- src/main/scala/arrays1d/sumAsFreq.scala
+++ src/main/scala/arrays1d/sumAsFreq.scala
@@ -130,54 +130,52 @@ object oneDsum extends App {
 
 
   //  println(System.currentTimeMillis() - long)
-//    println(sorted2)
+  //    println(sorted2)
 
   val summedMapped: Array[Long] = sorted2.map(_._2).toArray
 
 
-
   //    println("summed  " + (System.currentTimeMillis() - long))
 
-  def s(ix:Int) = if (ix > summedMapped.length - 1) 0l else summedMapped(ix)
-  val cache = Map(
-    (1, 1) -> (s(0)),
-    (1, 2) -> (s(0) + s(1)),
-    (1, 3) -> (s(0) + s(1) + s(2)),
-    (1, 4) -> (s(0) + s(1) + s(2) + s(3)),
-    (1, 5) -> (s(0) + s(1) + s(2) + s(3) + s(4)),
-    (1, 6) -> (s(0) + s(1) + s(2) + s(3) + s(4) + s(5)),
-    (1, 7) -> (s(0) + s(1) + s(2) + s(3) + s(4) + s(5) + s(6)),
-    (1, 8) -> (s(0) + s(1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7)),
-    (2, 2) -> (s(1)),
-    (2, 3) -> (s(1) + s(2)),
-    (2, 4) -> (s(1) + s(2) + s(3)),
-    (2, 5) -> (s(1) + s(2) + s(3) + s(4)),
-    (2, 6) -> (s(1) + s(2) + s(3) + s(4) + s(5)),
-    (2, 7) -> (s(1) + s(2) + s(3) + s(4) + s(5) + s(6)),
-    (2, 8) -> (s(1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7)),
-    (3, 3) -> (s(2)),
-    (3, 4) -> (s(2) + s(3)),
-    (3, 5) -> (s(2) + s(3) + s(4)),
-    (3, 6) -> (s(2) + s(3) + s(4) + s(5)),
-    (3, 7) -> (s(2) + s(3) + s(4) + s(5) + s(6)),
-    (3, 8) -> (s(2) + s(3) + s(4) + s(5) + s(6) + s(7)),
-    (4, 4) -> (s(3)),
-    (4, 5) -> (s(3) + s(4)),
-    (4, 6) -> (s(3) + s(4) + s(5)),
-    (4, 7) -> (s(3) + s(4) + s(5) + s(6)),
-    (4, 8) -> (s(3) + s(4) + s(5) + s(6) + s(7)),
-    (5, 5) -> (s(4)),
-    (5, 6) -> (s(4) + s(5)),
-    (5, 7) -> (s(4) + s(5) + s(6)),
-    (5, 8) -> (s(4) + s(5) + s(6) + s(7)),
-    (6, 6) -> (s(5)),
-    (6, 7) -> (s(5) + s(6)),
-    (6, 8) -> (s(5) + s(6) + s(7)),
-    (7, 7) -> (s(6)),
-    (7, 8) -> (s(6) + s(7)),
-    (8, 8) -> (s(7))
+  def s(ix: Int) = if (ix > summedMapped.length - 1) 0l else summedMapped(ix)
 
-  )
+  val cache11 = (s(0))
+  val cache12 = (s(0) + s(1))
+  val cache13 = (s(0) + s(1) + s(2))
+  val cache14 = (s(0) + s(1) + s(2) + s(3))
+  val cache15 = (s(0) + s(1) + s(2) + s(3) + s(4))
+  val cache16 = (s(0) + s(1) + s(2) + s(3) + s(4) + s(5))
+  val cache17 = (s(0) + s(1) + s(2) + s(3) + s(4) + s(5) + s(6))
+  val cache18 = (s(0) + s(1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7))
+  val cache22 = (s(1))
+  val cache23 = (s(1) + s(2))
+  val cache24 = (s(1) + s(2) + s(3))
+  val cache25 = (s(1) + s(2) + s(3) + s(4))
+  val cache26 = (s(1) + s(2) + s(3) + s(4) + s(5))
+  val cache27 = (s(1) + s(2) + s(3) + s(4) + s(5) + s(6))
+  val cache28 = (s(1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7))
+  val cache33 = (s(2))
+  val cache34 = (s(2) + s(3))
+  val cache35 = (s(2) + s(3) + s(4))
+  val cache36 = (s(2) + s(3) + s(4) + s(5))
+  val cache37 = (s(2) + s(3) + s(4) + s(5) + s(6))
+  val cache38 = (s(2) + s(3) + s(4) + s(5) + s(6) + s(7))
+  val cache44 = (s(3))
+  val cache45 = (s(3) + s(4))
+  val cache46 = (s(3) + s(4) + s(5))
+  val cache47 = (s(3) + s(4) + s(5) + s(6))
+  val cache48 = (s(3) + s(4) + s(5) + s(6) + s(7))
+  val cache55 = (s(4))
+  val cache56 = (s(4) + s(5))
+  val cache57 = (s(4) + s(5) + s(6))
+  val cache58 = (s(4) + s(5) + s(6) + s(7))
+  val cache66 = (s(5))
+  val cache67 = (s(5) + s(6))
+  val cache68 = (s(5) + s(6) + s(7))
+  val cache77 = (s(6))
+  val cache78 = (s(6) + s(7))
+  val cache88 = (s(7))
+
 
   val tests = scala.io.StdIn.readLine().toLong
 
@@ -192,9 +190,54 @@ object oneDsum extends App {
 
       var res: Long = 0l
 
-      if (cache.contains((freqFrom, Math.min(freqTo, summedMapped.length)))) {
-        res = cache((freqFrom, Math.min(freqTo, summedMapped.length)))
-      } else {
+      val fr = freqFrom
+      val to = Math.min(freqTo, summedMapped.length)
+
+      if (fr == 1 && to == 1) res = cache11
+      else if (fr == 1 && to == 2) res = cache12
+      else if (fr == 1 && to == 3) res = cache13
+      else if (fr == 1 && to == 4) res = cache14
+      else if (fr == 1 && to == 5) res = cache15
+      else if (fr == 1 && to == 6) res = cache16
+      else if (fr == 1 && to == 7) res = cache17
+      else if (fr == 1 && to == 8) res = cache18
+
+      else if (fr == 2 && to == 2) res = cache22
+      else if (fr == 2 && to == 3) res = cache23
+      else if (fr == 2 && to == 4) res = cache24
+      else if (fr == 2 && to == 5) res = cache25
+      else if (fr == 2 && to == 6) res = cache26
+      else if (fr == 2 && to == 7) res = cache27
+      else if (fr == 2 && to == 8) res = cache28
+
+      else if (fr == 3 && to == 3) res = cache33
+      else if (fr == 3 && to == 4) res = cache34
+      else if (fr == 3 && to == 5) res = cache35
+      else if (fr == 3 && to == 6) res = cache36
+      else if (fr == 3 && to == 7) res = cache37
+      else if (fr == 3 && to == 8) res = cache38
+
+      else if (fr == 4 && to == 4) res = cache44
+      else if (fr == 4 && to == 5) res = cache45
+      else if (fr == 4 && to == 6) res = cache46
+      else if (fr == 4 && to == 7) res = cache47
+      else if (fr == 4 && to == 8) res = cache48
+
+      else if (fr == 5 && to == 5) res = cache55
+      else if (fr == 5 && to == 6) res = cache56
+      else if (fr == 5 && to == 7) res = cache57
+      else if (fr == 5 && to == 8) res = cache58
+
+      else if (fr == 6 && to == 6) res = cache66
+      else if (fr == 6 && to == 7) res = cache67
+      else if (fr == 6 && to == 8) res = cache68
+
+      else if (fr == 7 && to == 7) res = cache77
+      else if (fr == 7 && to == 8) res = cache78
+
+      else if (fr == 8 && to == 8) res = cache88
+
+      else {
         for (i <- freqFrom - 1 to Math.min(freqTo, summedMapped.length) - 1) {
           println("xx")
           res = res + summedMapped(i)
