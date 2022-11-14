bc6353a4ef1e1803ab60a9be3773d3e18631e5a4
Andrejs Olins <andrejs.olins@gmail.com>
2022-11-14 22:17:04 +0200

    non decreasing

---
+++ src/main/scala/arrays1d/nondecreasing.scala
@@ -0,0 +1,32 @@
+package arrays1d
+
+object  nondecreasing extends App {
+
+  val long = System.currentTimeMillis()
+
+  //https://www.hackerearth.com/practice/data-structures/arrays/1-d/practice-problems/algorithm/make-it-non-decreasing-7d3391fd/
+  val tests = scala.io.StdIn.readLine().toInt
+
+  for (_ <- 1 to tests) {
+    val length = scala.io.StdIn.readLine().toInt
+    var array:Array[Long] = scala.io.StdIn.readLine().split(" ").map(_.toLong)
+
+    for (ix <- 1 until length){
+      val previous = array(ix - 1)
+      val current = array(ix)
+
+      if (current < previous){
+        val multiplicator = previous / current
+        val newCurrent = current * multiplicator
+        if (newCurrent >= previous){
+          array(ix) = newCurrent
+        }else{
+          array(ix) = newCurrent + current
+        }
+
+      }
+    }
+
+    println(array.mkString(" "))
+  }
+}
